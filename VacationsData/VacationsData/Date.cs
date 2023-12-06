544e5f92a73dfb51d69f316c897129acc12ea2a4
Nastya <nastya.eremina.98@gmail.com>
2023-12-06 15:03:03 +0300

    Add scetch

---
+++ VacationsData/VacationsData/Date.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsData
+{
+    public class Date
+    {
+        public bool CanCreateVacationProp { get; set; }
+        public bool ExistStartProp { get; set; }
+        public bool ExistEndProp { get; set; }
+
+        public List<DateTime> Vacations = new List<DateTime>();
+        public List<DateTime> DatesList = new List<DateTime>();
+        public List<DateTime> SetDatesList = new List<DateTime>();
+        public Random RandomGen = new Random();
+       
+        private List<string> _aviableWorkingDaysOfWeekWithoutWeekends = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+        private int[] _vacationSteps = { 7, 14 };
+        private int _vacationCountProp = 28;
+        private int _difference = 0;
+        private int _startMonth = 1;
+        private int _startDay = 1;
+        private int _endMonth = 12;
+        private int _endDay = 31;
+
+        public List<string> AviableWorkingDaysOfWeekWithoutWeekends => _aviableWorkingDaysOfWeekWithoutWeekends;
+        public int[] VacationSteps => _vacationSteps;
+        public int VacationCountProp => _vacationCountProp;
+        public int Difference => _difference;
+        public int StartMonth => _startMonth;
+        public int StartDay => _startDay;
+        public int EndMonth => _endMonth;
+        public int EndDay => _endDay;
+    }
+}
---
+++ VacationsData/VacationsData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationsData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            VacationsEmployee vacationsEmployee = new VacationsEmployee();
+
+            vacationsEmployee.ShowVacationsEmployee();
+        }
+    }
+}
